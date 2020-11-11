cd75002843aacb3b8b7bd9197de2f309afce1822
luis mori <lguerra@contractor.linuxfoundation.org>
2020-11-11 13:47:36 -0500

    adding net module

--- bin/cdk-typescript.ts
+++ bin/cdk-typescript.ts
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkTypescriptStack } from '../lib/cdk-typescript-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { CdkTypescriptStack } from "../lib/cdk-typescript-stack";
+import { Tags } from "@aws-cdk/core";
 
 const app = new cdk.App();
-new CdkTypescriptStack(app, 'CdkTypescriptStack');
+const stack = new CdkTypescriptStack(app, "CdkTypescriptStack", {
+  env: {
+    region: "us-west-1",
+  },
+});
+
+Tags.of(stack).add("App", "Lguerra-test-documents");
+Tags.of(stack).add("Environment", "Lguerra-test-dev");
--- lib/cdk-typescript-stack.ts
+++ lib/cdk-typescript-stack.ts
@@ -1,9 +1,25 @@
-import * as cdk from '@aws-cdk/core';
+import { Bucket, BucketEncryption } from "@aws-cdk/aws-s3";
+import * as cdk from "@aws-cdk/core";
+import { Tags } from "@aws-cdk/core";
+import { Networking } from "./networking";
 
 export class CdkTypescriptStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, "DocumentBucket", {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+
+    new cdk.CfnOutput(this, "DocumentsBucketNameExport", {
+      value: bucket.bucketName,
+      exportName: "DocumentsBucketName",
+    });
+
+    const networking = new Networking(this, "NetworkingConstruct", {
+      maxAzs: 2,
+    });
+
+    Tags.of(networking).add("Module", "networking");
   }
 }
