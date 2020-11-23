cc08ba1d51022254ae1276943bb46cdbf94778a8
luis mori <lguerra@contractor.linuxfoundation.org>
2020-11-23 08:41:28 -0500

    adding api g integration

--- api/getDocuments/index.ts
+++ api/getDocuments/index.ts
@@ -4,6 +4,10 @@ import {
   Context,
 } from "aws-lambda";
 
+import S3 from "aws-sdk/clients/s3";
+
+const s3 = new S3();
+
 const bucketName = process.env.DOCUMENTS_BUCKET_NAME;
 
 export const getDocuments = async (
@@ -12,8 +16,38 @@ export const getDocuments = async (
 ): Promise<APIGatewayProxyStructuredResultV2> => {
   console.log(`Bucket name : ${bucketName}`);
 
+  try {
+    const { Contents: results } = await s3
+      .listObjects({
+        Bucket: process.env.DOCUMENTS_BUCKET_NAME!,
+      })
+      .promise();
+    const documents = await Promise.all(
+      results!.map(async (r) => generateSignedURL(r))
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(documents),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.message,
+    };
+  }
+};
+
+const generateSignedURL = async (
+  object: S3.Object
+): Promise<{ filename: string; url: string }> => {
+  const url = await s3.getSignedUrlPromise("getObject", {
+    Bucket: bucketName,
+    Key: object.Key!,
+    Expires: 60 * 60,
+  });
+
   return {
-    statusCode: 200,
-    body: "Success",
+    filename: object.Key!,
+    url,
   };
 };
--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,14 @@
 import * as cdk from "@aws-cdk/core";
-import * as lambda from '@aws-cdk/aws-lambda-nodejs';
+import * as lambda from "@aws-cdk/aws-lambda-nodejs";
 import { Runtime } from "@aws-cdk/aws-lambda";
-import * as path from 'path';
-import * as s3 from '@aws-cdk/aws-s3';
+import * as path from "path";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as iam from "@aws-cdk/aws-iam";
+import * as apig from "@aws-cdk/aws-apigatewayv2";
+import { LambdaProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
 
 interface DocumentManagementAPIProps {
-  documentBucket: s3.IBucket
+  documentBucket: s3.IBucket;
 }
 
 export class DocumentManagementAPI extends cdk.Construct {
@@ -16,16 +19,59 @@ export class DocumentManagementAPI extends cdk.Construct {
   ) {
     super(scope, id);
 
-    const getDocumentsFunction = new lambda.NodejsFunction(this, 'GetDocumentsFunction', {
-      runtime: Runtime.NODEJS_12_X,
-      entry: path.join(__dirname, '..', 'api', 'getDocuments', 'index.ts'),
-      handler: 'getDocuments',
-      externalModules: [
-        'aws-sdk'
-      ],
-      environment: {
-        DOCUMENTS_BUCKET_NAME: props.documentBucket.bucketName
+    const getDocumentsFunction = new lambda.NodejsFunction(
+      this,
+      "GetDocumentsFunction",
+      {
+        runtime: Runtime.NODEJS_12_X,
+        entry: path.join(__dirname, "..", "api", "getDocuments", "index.ts"),
+        handler: "getDocuments",
+        externalModules: ["aws-sdk"],
+        environment: {
+          DOCUMENTS_BUCKET_NAME: props.documentBucket.bucketName,
+        },
       }
+    );
+
+    const bucketPermissions = new iam.PolicyStatement();
+    bucketPermissions.addResources(`${props.documentBucket.bucketArn}/*`);
+    bucketPermissions.addActions("s3:GetObject", "s3:PutObject");
+
+    getDocumentsFunction.addToRolePolicy(bucketPermissions);
+
+    const bucketContainerPermissions = new iam.PolicyStatement();
+    bucketContainerPermissions.addResources(
+      `${props.documentBucket.bucketArn}`
+    );
+    bucketContainerPermissions.addActions("s3:ListBucket");
+
+    getDocumentsFunction.addToRolePolicy(bucketContainerPermissions);
+
+    const httpapi = new apig.HttpApi(this, "HttpAPI", {
+      apiName: "document-management-api",
+      createDefaultStage: true,
+      corsPreflight: {
+        allowMethods: [apig.HttpMethod.GET],
+        allowOrigins: ["*"],
+        maxAge: cdk.Duration.days(10),
+      },
+    });
+
+
+    const integration = new LambdaProxyIntegration({
+      handler: getDocumentsFunction,
+    });
+
+    httpapi.addRoutes({
+      path: '/getDocuments',
+      methods: [apig.HttpMethod.GET],
+      integration: integration
+    });
+
+    new cdk.CfnOutput(this, 'APIEndpoint', {
+      value: httpapi.url!,
+      exportName: 'APIEndpoint'
     })
+
   }
 }
